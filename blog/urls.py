4567fcd2b65ee2d14b97811a9414be2878b8fa01
davidg <dgalludec@gmail.com>
2021-01-22 14:07:11 +0000

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 10:34:06 2021
+
+@author: Multiple Monitors
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
